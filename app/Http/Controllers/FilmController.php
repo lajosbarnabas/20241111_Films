45d31686454a2ede501aa1fcfc81e8dd81dd145b
lajosbarnabas0 <lajosbarnabas0@gmail.com>
2024-11-11 12:19:59 +0100

    controller routeok létrehozása

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -3,8 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FilmController extends Controller
 {
-    //
+    public $films;
+
+    function __construct()
+    {
+        $this->films = Storage::json('filmek_2.json');
+        // dd($this->films);
+    }
+
+    public function index()
+    {
+        return view('films', ['films' => $this->films]);
+    }
+
+    public function show($id)
+    {
+        foreach ($this->films as $film) {
+            if ($film['id'] == $id) {
+                return view('film', ['film' => $film]);
+            }
+        }
+        return abort(404);
+    }
+
+    public function directors()
+    {
+        $directors = [];
+        foreach ($this->films as $film) {
+            $d = explode(',', $film['Rendezo']);
+            foreach ($d as $item) {
+                if (!in_array($item, $directors)) {
+                    $directors[] = $item;
+                }
+            }
+        }
+
+        sort($directors);
+
+        return view('directors', ['directors' => $directors]);
+    }
+
+    public function filmsOfDirector($director) {
+        $films = [];
+        foreach ($this->films as $film) {
+            if (str_contains($film['Rendezo'], $director)) {
+                $films[] = $film;
+            }
+        }
+
+        return view('films', ['films' => $films]);
+    }
+
+    public function searchByTitle(Request $request) {
+        $films = [];
+        foreach ($this->films as $film) {
+            if (str_contains(strtolower($film['Cim_en']), strtolower($request->title))) {
+                $films[] = $film;
+            }
+        }
+
+        $request->flash();
+
+        return view('films', ['films' => $films]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\FilmController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/films');
+Route::get('/films', [FilmController::class, 'index']);
+Route::get('/films/{id}', [FilmController::class, 'show']);
+
+Route::get('/directors', [FilmController::class, 'directors']);
+Route::get('/directors/{director}', [FilmController::class, 'filmsOfDirector']);
+
+Route::post('/films/search-by-title', [FilmController::class, 'searchByTitle']);
